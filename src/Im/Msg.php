18196ca66f52a189a997bda24a51ba469da6961d
yangminghui <guanhui07@gmail.com>
2024-10-30 11:30:40 +0800

    add

--- src/Im/Msg.php
+++ src/Im/Msg.php
@@ -9,6 +9,7 @@ use GuzzleHttp\Exception\GuzzleException;
 /**
  * 消息管理
  * @see https://doc.yunxin.163.com/messaging/server-apis/zk4Mzg0MjE?platform=server#消息功能
+ * @see https://doc.yunxin.163.com/messaging/server-apis/zk4Mzg0MjE?platform=server#历史消息与记录
  */
 class Msg extends Base
 {
