4c45e6c728232616008fd678c37a5d5ddf3fbcee
guanhui07 <guanhui07@gmail.com>
2025-01-15 18:25:49 +0800

    add

---
+++ src/Im/Room.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace cccdl\yunxin_sdk\Im;
+
+use cccdl\yunxin_sdk\Exception\cccdlException;
+use GuzzleHttp\Exception\GuzzleException;
+
+/**
+ * @see https://doc.yunxin.163.com/nertc/server-apis/TE0NDI4MjY?platform=server 房间管理
+ * 创建频道
+ * @see https://doc.yunxin.163.com/live-streaming/server-apis/TkzNzkzNTk?platform=server#%E5%88%9B%E5%BB%BA%E9%A2%91%E9%81%93
+ */
+class Room extends Base
+{
+
+    /**
+     * 创建音视频房间
+     * @see https://doc.yunxin.163.com/nertc/server-apis/jg3NjcyNTE?platform=server
+     */
+    public function createRoom($channelName, $uid): array
+    {
+        $url = 'https://logic-dev.netease.im/v2/api/room';
+        $data = [
+            'channelName' => $channelName,
+            'mode' => 2,
+            'uid' => $uid,
+        ];
+        return $this->postV2($url, $data);
+
+    }
+
+    /**
+     * 创建频道
+     * @param string $name
+     * @param int $type
+     * @return array
+     * @see https://doc.yunxin.163.com/live-streaming/server-apis/TkzNzkzNTk?platform=server#%E5%88%9B%E5%BB%BA%E9%A2%91%E9%81%93
+     *
+     */
+    public function createChannel(string $name, int $type = 0): array
+    {
+        $url = 'https://vcloud.163.com/app/channel/create';
+        $data = [
+            'name' => $name,
+            'type' => $type,
+        ];
+        return $this->postV2($url, $data);
+
+    }
+
+
+
+    /**
+     * 创建推流任务
+     * @param int $user_id
+     * @param string $cname
+     * @param string $task_id
+     * @param string $stream_url
+     * @param  $layout
+     * @return array|bool|int|string
+     * https://doc.yunxin.163.com/nertc/server-apis/DM0MTg2NTM?platform=server
+     */
+    public function createStreamTaskV3(int $user_id, string $cname, string $task_id, string $stream_url,  $layout)
+    {
+        $url = sprintf('https://logic-dev.netease.im/v3/api/rooms/task?cname=%s', $cname);
+        $data = [
+            'hostuid' => $user_id,
+            'taskId' => $task_id,
+            'streamUrl' => $stream_url,
+            'layout' => $layout,
+            'version' => 1,
+        ];
+        return $this->postV2($url, $data);
+    }
+
+    /**
+     * 创建推流任务
+     * @param int $user_id
+     * @param string $cid
+     * @param string $task_id
+     * @param string $stream_url
+     * @param  $layout
+     * @return array|bool|int|string
+     * https://doc.yunxin.163.com/nertc/server-apis/zQ0MDkzNDI?platform=server
+     */
+    public function createStreamTaskV3ByCid(int $user_id, string $cid, string $task_id, string $stream_url, $layout)
+    {
+        $url = sprintf('https://logic-dev.netease.im/v2/api/rooms/%s/task', $cid);
+        $data =[
+            'hostuid' => $user_id,
+            'taskId' => $task_id,
+            'streamUrl' => $stream_url,
+            'layout' => $layout,
+            'version' => 1,
+        ];
+        return $this->postV2($url, $data);
+
+    }
+
+
+    /**
+     * 查看推流任务
+     * @param string $cname
+     * @param string $task_id
+     * @return array|bool|int|string
+     * https://doc.yunxin.163.com/nertc/server-apis/jE5OTE5NDA?platform=server
+     */
+    public function getStreamTaskV3(string $cname, string $task_id)
+    {
+        $url = sprintf('https://logic-dev.netease.im/v3/api/rooms/task?cname=%s&taskId=%s', $cname, $task_id);
+        $res = $this->get($url, "", 'GET', true);
+
+        return $res;
+    }
+
+
+
+
+    /**
+     * 更新推流任务
+     * @param int $user_id
+     * @param string $cname
+     * @param string $task_id
+     * @param string $stream_url
+     * @param $layout
+     * @param bool $record
+     * @param $is_all_audio
+     * @return mixed
+     * @throws cccdlException
+     * https://doc.yunxin.163.com/nertc/server-apis/zQ0MDkzNDI?platform=server
+     */
+    public function updateStreamTaskV3(int $user_id, string $cname, string $task_id, string $stream_url,  $layout, bool $record = false, $is_all_audio = false)
+    {
+        $url = sprintf('https://logic-dev.netease.im/v3/api/rooms/update/task?cname=%s', $cname);
+        $data = [
+            'taskId' => $task_id,
+            'streamUrl' => $stream_url,
+            'layout' => $layout,
+            'record' => $record,
+            'hostuid' => $user_id,
+        ];
+        if($is_all_audio){
+            $data['config'] = ['subAllAudio' => true];
+            $data['version'] = 1;
+        }
+        return $this->postV2($url, $data);
+    }
+
+}
--- src/Traits/Request.php
+++ src/Traits/Request.php
@@ -44,4 +44,65 @@ trait Request
 
         return $arr;
     }
+
+    public function postV2($url, $param)
+    {
+
+        $time = time();
+        $parsed =  parse_url($url);
+        $client = new Client([
+            'base_uri' => $parsed['scheme'].'://'.$parsed['host'].'/',
+            'timeout' => self::$timeout,
+            'headers' => [
+                'AppKey' => $this->AppKey,
+                'Nonce' => $this->Nonce,
+                'CurTime' => $time,
+                'CheckSum' => sha1($this->AppSecret . $this->Nonce . $time),
+            ]
+        ]);
+
+        $response = $client->post($parsed['path'], ['form_params' => $param]);
+
+        if ($response->getStatusCode() != 200) {
+            throw new cccdlException('请求失败: ' . $response->getStatusCode());
+        }
+
+        $arr = json_decode($response->getBody(), true);
+
+        if (!isset($arr['code']) || $arr['code'] != 200) {
+            throw new cccdlException('请求结果异常' . $response->getBody());
+        }
+
+        return $arr;
+    }
+
+    public function get($url)
+    {
+        $time = time();
+        $parsed =  parse_url($url);
+        $client = new Client([
+            'base_uri' => $parsed['scheme'].'://'.$parsed['host'].'/',
+            'timeout' => self::$timeout,
+            'headers' => [
+                'AppKey' => $this->AppKey,
+                'Nonce' => $this->Nonce,
+                'CurTime' => $time,
+                'CheckSum' => sha1($this->AppSecret . $this->Nonce . $time),
+            ]
+        ]);
+
+        $response = $client->get($parsed['path']);
+
+        if ($response->getStatusCode() != 200) {
+            throw new cccdlException('请求失败: ' . $response->getStatusCode());
+        }
+
+        $arr = json_decode($response->getBody(), true);
+
+        if (!isset($arr['code']) || $arr['code'] != 200) {
+            throw new cccdlException('请求结果异常' . $response->getBody());
+        }
+
+        return $arr;
+    }
 }
