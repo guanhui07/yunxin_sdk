294d7497105defc0c32d25f321b36d1c8efef72f
guanhui07 <guanhui07@gmail.com>
2025-01-16 14:25:28 +0800

    add

--- src/Im/Chatroom.php
+++ src/Im/Chatroom.php
@@ -27,6 +27,7 @@ class Chatroom extends Base
      * @return array
      * @throws GuzzleException
      * @throws cccdlException
+     * @see https://doc.yunxin.163.com/messaging/server-apis/jY2MDg1MTk?platform=server
      */
     public function sendMsg(int $roomId, string $fromAccid, int $msgType, array $options = []): array
     {
@@ -53,8 +54,9 @@ class Chatroom extends Base
      * @return mixed
      * @throws GuzzleException
      * @throws cccdlException
+     * @see https://doc.yunxin.163.com/messaging/docs/DkzNTA5NTI?platform=server
      */
-    public function sendRoomToMsg(int $fromAccid,array $toAccids,int $roomid,array $attach,int $msgId, $options = [], $ext = "")
+    public function sendRoomToMsg(int $fromAccid, array $toAccids, int $roomid, array $attach, int $msgId, $options = [], $ext = "")
     {
         $data = [
             'roomid' => $roomid,
@@ -85,8 +87,9 @@ class Chatroom extends Base
      * @return mixed
      * @throws GuzzleException
      * @throws cccdlException
+     * @see https://doc.yunxin.163.com/messaging/docs/zc1MTY2NDQ?platform=server
      */
-    public function sendRoomBroadcast(array $attach,int $msgId,int $fromAccid,array $options = [],array $ext = [])
+    public function sendRoomBroadcast(array $attach, int $msgId, int $fromAccid, array $options = [], array $ext = [])
     {
         $data = [
             'msgId' => $msgId,
@@ -106,6 +109,56 @@ class Chatroom extends Base
         }
 
         return $this->post('chatroom/broadcast.action', array_merge($options, $data));
+    }
+
+    /**
+     * 聊天室用户ids是否在线
+     * @see https://doc.yunxin.163.com/docs/TM5MzM5Njk/TYyMDI1MTg?platformId=60353#批量获取在线成员信息
+     */
+    public function queryMembers(int $roomId, array $accids)
+    {
+        $data = [
+            'roomid' => $roomId,
+            'accids' => json_encode($accids),
+        ];
+
+        return $this->post('chatroom/queryMembers.action', $data);
+    }
+
+    /**
+     * 聊天室用户 列表
+     * @see https://doc.yunxin.163.com/docs/TM5MzM5Njk/TYyMDI1MTg?platformId=60353#分页获取成员列表
+     * @param $type 0:固定成员;1:非固定成员;2:仅返回在线的固定成员
+     * @see https://doc.yunxin.163.com/messaging/server-apis/zAzMjUxMzc?platform=server
+     *
+     */
+    public function membersByPage(int $roomId, int $endTime = 0, int $type = 1, int $perPage = 100)
+    {
+        $data = [
+            'roomid' => $roomId,
+            'endtime' => $endTime,
+            'type' => $type,
+            'limit' => $perPage,
+        ];
+
+        return $this->post('chatroom/membersByPage.action', $data);
+    }
+
+    /**
+     * 创建聊天室
+     * @see https://doc.yunxin.163.com/messaging/server-apis/jA0MzQxOTI?platform=server
+     */
+    public function addChatRoom($creator, $name, $announcement = '', $broadcasturl = '', $ext = '', $queuelevel = 0)
+    {
+        $data = [
+            'creator' => $creator,
+            'name' => $name,
+            'announcement' => $announcement,
+            'broadcasturl' => $broadcasturl,
+            'ext' => $ext,
+            'queuelevel' => $queuelevel,
+        ];
+        return $this->post('chatroom/create.action', $data);
     }
 
 }
